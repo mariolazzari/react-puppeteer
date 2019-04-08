3f7e27392316f2d0dbbc2b34f58999182528a374
mariolazzari <mario.lazzari@gmail.com>
2019-04-08 21:29:46 +0200

    created pdf

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const puppeteer = require("puppeteer");
+
+const createPdf = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const options = {
+    path: "web.pdf",
+    format: "A4"
+  };
+
+  try {
+    await page.goto("https://solutionmap.it", { waitUntil: "networkidle2" });
+    await page.pdf(options);
+
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createPdf();
 
 // read enviroment variables
 require("dotenv").config();
